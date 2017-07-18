c6242128ffa7988599bc03347ffdbbf6abdd2e59
Ed <edmccormic@gmail.com>
2017-07-18 12:32:21 -0600

    Initial Commit

---
+++ Thumbup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thumbup
+//
+//  Created by Ed McCormic on 7/18/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
